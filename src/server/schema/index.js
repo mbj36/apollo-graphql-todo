5e2a7f292b338dc6163a78f94df17d4eac5bb1c0
mbj36 <mohitbajo36@gmail.com>
2018-03-15 15:15:34 +0530

    GraphQL server

--- src/server/schema/index.js
+++ src/server/schema/index.js
@@ -1,1 +1,10 @@
 import { makeExecutableSchema } from 'graphql-tools'
+import Resolvers from '../resolvers'
+import Schema from './schema'
+
+const executableSchema = makeExecutableSchema({
+  typeDefs: Schema,
+  resolvers: Resolvers
+})
+
+export default executableSchema
