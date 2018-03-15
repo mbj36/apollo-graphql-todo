78b6c36fd70ddf68025ee9695d5a88c2274f8d93
mbj36 <mohitbajo36@gmail.com>
2018-03-15 16:07:45 +0530

    Apollo client

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './client/index.css'
 import App from './client/components/App'
 import registerServiceWorker from './client/registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { ApolloProvider } from 'react-apollo'
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
