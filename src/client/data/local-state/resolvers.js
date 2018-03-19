384c0f21ce32a7427196c81dced302b6bde15176
mbj36 <mohitbajo36@gmail.com>
2018-03-19 10:03:59 +0530

    Adding mutations

--- src/client/data/local-state/resolvers.js
+++ src/client/data/local-state/resolvers.js
@@ -10,3 +10,46 @@ const todosQuery = gql`
     }
   }
 `
+export default {
+  Mutation: {
+    addTodo: (_, variables, { cache }) => {
+      const { todos } = cache.readQuery({
+        query: todosQuery
+      })
+      const data = {
+        todos: [
+          {
+            id: uuid.v4(),
+            title: variables.title,
+            completed: false,
+            __typename: todo
+          },
+          ...todos
+        ]
+      }
+      cache.writeData({ data })
+      return null
+    },
+    deleteTodo: (_, variables, { cache }) => {
+      const { todos } = cache.readQuery({ query: readQuery })
+      const data = {
+        todos: todos.filter(todo => todo.id !== variables.id)
+      }
+      cache.writeData({ data })
+      return null
+    },
+    toggleTodo: (_, variables, { cache }) => {
+      const { todos } = cache.readQuery({
+        query: todosQuery
+      })
+      const data = {
+        todos: todos.map(todo => ({
+          ...todo,
+          completed: todo.id === variables.id ? !todo.completed : todo.completed
+        }))
+      }
+      cache.writeData({ data })
+      return null
+    }
+  }
+}
